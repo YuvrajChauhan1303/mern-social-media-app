87e6a2e33d718a376a7a9fb6d74c2075d18d1a14
YuvrajChauhan1303 <email.yuvraj.13@gmail.com>
2024-07-02 15:17:44 +0530

    added routes and controllers for auth

---
+++ server/controllers/auth.controller.js
@@ -0,0 +1,16 @@
+export const signup = async (req, res) => {
+    res.json({
+        data: "You Hit Signup Endpoint"
+    })
+}
+
+export const login = async (req, res) => {
+    res.json({
+        data: "You Hit Login Endpoint"
+    })
+}
+export const logout = async (req, res) => {
+    res.json({
+        data: "You Hit Logout  Endpoint"
+    })
+}
---
+++ server/server.js
@@ -0,0 +1,11 @@
+import express from "express"
+
+import authRoutes from "./routes/auth.routes.js"
+
+const app = express()
+
+app.use("api/auth/", authRoutes)
+
+app.listen(8000 , () => {
+    console.log("Server running on Port 8000")
+})
