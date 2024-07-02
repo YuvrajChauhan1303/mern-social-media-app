cdef8a90811b395117e23527553c841171822212
YuvrajChauhan1303 <email.yuvraj.13@gmail.com>
2024-07-02 22:07:17 +0530

    getMe controller and protectRoute middleware added

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,11 +1,13 @@
-import express from "express"
-import { login, logout, signup } from "../controllers/auth.controller.js"
+import express from "express";
+import { login, logout, signup } from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/protectRoute.js";
+import { getMe } from "../controllers/auth.controller.js";
 
+const router = express.Router();
 
-const router = express.Router()
-
-router.post("/signup", signup)
-router.post("/login", login)
-router.post("/logout", logout)
+router.post("/signup", signup);
+router.post("/login", login);
+router.post("/logout", logout);
+router.get("/me", protectRoute, getMe);
 
 export default router;
--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 import express from "express"
 import dotenv from "dotenv"
-
+import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.routes.js"
 import connectMongoDB from "./db/connectMongo.js";
 
@@ -11,6 +11,7 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
+app.use(cookieParser())
 
 app.use("/api/auth", authRoutes)
 
